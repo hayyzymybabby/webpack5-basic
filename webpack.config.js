689794114e9d36cd797eae713980ebc38502dbb6
usamihal <usamihal001@163.com>
2022-01-13 01:24:03 +0800

    20220113

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'none',
+  // 模式
+  mode: 'development',
+  // 入口
   entry: './src/index.js',
   output: {
+    // 打包后的文件夹
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    // 打包后路径
+    path: path.resolve(__dirname, './dist'),
+    // 每次打包后清除原文件
+    clean: true
+  },
+  // source-map定位错误信息
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 使用的html模板
+      template: './index.html',
+      // script插入的位置
+      inject: 'body'
+    })
+  ],
+  devServer: {
+    static: './dist'
   }
 }
